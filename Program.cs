f4af55c91bc30f50121f077e539ab5ddb44d2336
MarcelinoOliveira <101429111+marcelinooliveira@users.noreply.github.com>
2022-05-06 18:20:24 -0300

    Authentication and Authorization Basic Configs.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,33 @@
+using System.Text;
 using BlogEF.Data;
+using BlogVisualStudio;
+using BlogVisualStudio.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var key = Encoding.ASCII.GetBytes(Configuration.JwTKey);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 builder.Services.AddControllers()
     .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
 builder.Services.AddDbContext<VSBlogDataContext>();
+builder.Services.AddTransient<TokenService>();
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
