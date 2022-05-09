df1e3a54570fb25ab5874b2d5b1aefd20804656e
MarcelinOliveira <104645737+marcelinoliveira@users.noreply.github.com>
2022-05-09 14:40:32 -0300

    Recovering Lost Files After VersionError

---
+++ Services/TokenService.cs
@@ -0,0 +1,18 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using BlogVisualStudio.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BlogVisualStudio.Services;
+
+public class TokenService
+{
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var tokenDescriptor = new SecurityTokenDescriptor();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
