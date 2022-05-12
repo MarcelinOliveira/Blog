8b9c6b12a0cd645bf5c3dc911589bb764a4d7ced
MarcelinOliveira <104645737+marcelinoliveira@users.noreply.github.com>
2022-05-12 18:07:15 -0300

    Basic Configs for sing in and login Password Hash Save on Database Token Creation and authentication.

---
+++ Migrations/20220511203305_UserMigration2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogVisualStudio.Migrations
+{
+    public partial class UserMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "GitHub",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastUpdateDate",
+                table: "Post",
+                type: "SMALLDATETIME",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: new DateTime(2022, 5, 11, 20, 33, 5, 702, DateTimeKind.Utc).AddTicks(926),
+                oldClrType: typeof(DateTime),
+                oldType: "SMALLDATETIME",
+                oldMaxLength: 60,
+                oldDefaultValue: new DateTime(2022, 5, 11, 20, 29, 7, 526, DateTimeKind.Utc).AddTicks(6506));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "GitHub",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastUpdateDate",
+                table: "Post",
+                type: "SMALLDATETIME",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: new DateTime(2022, 5, 11, 20, 29, 7, 526, DateTimeKind.Utc).AddTicks(6506),
+                oldClrType: typeof(DateTime),
+                oldType: "SMALLDATETIME",
+                oldMaxLength: 60,
+                oldDefaultValue: new DateTime(2022, 5, 11, 20, 33, 5, 702, DateTimeKind.Utc).AddTicks(926));
+        }
+    }
+}
